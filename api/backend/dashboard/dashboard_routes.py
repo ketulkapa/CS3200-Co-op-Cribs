5b473f7ef4896f8a26af5675c0dc9c5aed3c4d0a
cathreenp <paul.cat@northeastern.edu>
2024-12-03 16:49:52 -0500

    added route to update an existing dashboard

--- api/backend/dashboard/dashboard_routes.py
+++ api/backend/dashboard/dashboard_routes.py
@@ -37,3 +37,30 @@ def get_dashboard(dashboard_id):
     the_response.status_code = 200
     return the_response
 
+# Update an existing dashboard by ID
+@dashboard.route('/dashboard/<int:dashboard_id>', methods=['PUT'])
+def update_dashboard(dashboard_id):
+    current_app.logger.info(f'PUT /dashboard/{dashboard_id} route')
+    dashboard_info = request.json
+    seasonal_trend = dashboard_info.get('seasonal_trend', None)
+    vacancy_rate = dashboard_info.get('vacancy_rate', None)
+    safety_flag = dashboard_info.get('safety_flag', None)
+    demand_forecast = dashboard_info.get('demand_forecast', None)
+    neighborhood = dashboard_info.get('neighborhood', None)
+    
+    query = '''
+        UPDATE dashboard
+        SET seasonal_trend = %s, vacancy_rate = %s, safety_flag = %s, demand_forecast = %s, neighborhood = %s
+        WHERE dashboard_id = %s
+    '''
+
+    data = (seasonal_trend, vacancy_rate, safety_flag, demand_forecast, neighborhood, dashboard_id)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+
+    response = make_response(jsonify({'message': 'Dashboard updated successfully!'}))
+    response.status_code = 200
+    return response
+
+
