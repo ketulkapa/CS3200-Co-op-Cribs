06a1ae9e72e7c3bf12cf6b36b7ed4b2ad977338d
ketulkapa <kapadia.ket@northeastern.edu>
2024-12-02 20:30:56 -0500

    added listing get route and listing get by id route

--- api/backend/listings/listings_routes.py
+++ api/backend/listings/listings_routes.py
@@ -19,12 +19,11 @@ listings = Blueprint('listings', __name__)
 #------------------------------------------------------------
 # Get all customers from the system
 @listings.route('/listings', methods=['GET'])
-def get_customers():
+def get_listings():
 
     cursor = db.get_db().cursor()
     cursor.execute('''
-                   SELECT id, company, last_name,
-                    first_name, job_title, business_phone FROM customers
+                    SELECT * FROM listings
     ''')
     
     theData = cursor.fetchall()
@@ -33,52 +32,64 @@ def get_customers():
     the_response.status_code = 200
     return the_response
 
-#------------------------------------------------------------
-# Update customer info for customer with particular userID
-#   Notice the manner of constructing the query.
-@customers.route('/customers', methods=['PUT'])
-def update_customer():
-    current_app.logger.info('PUT /customers route')
-    cust_info = request.json
-    cust_id = cust_info['id']
-    first = cust_info['first_name']
-    last = cust_info['last_name']
-    company = cust_info['company']
-
-    query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
-    data = (first, last, company, cust_id)
+@listings.route('listing/<listingID>', methods=['GET'])
+def get_listing(listingID):
     cursor = db.get_db().cursor()
-    r = cursor.execute(query, data)
-    db.get_db().commit()
-    return 'customer updated!'
-
-#------------------------------------------------------------
-# Get customer detail for customer with particular userID
-#   Notice the manner of constructing the query. 
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
-    current_app.logger.info('GET /customers/<userID> route')
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT id, first_name, last_name FROM customers WHERE id = {0}'.format(userID))
+    cursor.execute('''
+                    SELECT * FROM listings WHERE id = {0}
+    '''.format(listingID))
     
     theData = cursor.fetchall()
     
     the_response = make_response(jsonify(theData))
     the_response.status_code = 200
     return the_response
+# #------------------------------------------------------------
+# # Update customer info for customer with particular userID
+# #   Notice the manner of constructing the query.
+# @listings.route('/customers', methods=['PUT'])
+# def update_customer():
+#     current_app.logger.info('PUT /customers route')
+#     cust_info = request.json
+#     cust_id = cust_info['id']
+#     first = cust_info['first_name']
+#     last = cust_info['last_name']
+#     company = cust_info['company']
 
-#------------------------------------------------------------
-# Makes use of the very simple ML model in to predict a value
-# and returns it to the user
-@customers.route('/prediction/<var01>/<var02>', methods=['GET'])
-def predict_value(var01, var02):
-    current_app.logger.info(f'var01 = {var01}')
-    current_app.logger.info(f'var02 = {var02}')
+#     query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
+#     data = (first, last, company, cust_id)
+#     cursor = db.get_db().cursor()
+#     r = cursor.execute(query, data)
+#     db.get_db().commit()
+#     return 'customer updated!'
 
-    returnVal = predict(var01, var02)
-    return_dict = {'result': returnVal}
+# #------------------------------------------------------------
+# # Get customer detail for customer with particular userID
+# #   Notice the manner of constructing the query. 
+# @customers.route('/customers/<userID>', methods=['GET'])
+# def get_customer(userID):
+#     current_app.logger.info('GET /customers/<userID> route')
+#     cursor = db.get_db().cursor()
+#     cursor.execute('SELECT id, first_name, last_name FROM customers WHERE id = {0}'.format(userID))
+    
+#     theData = cursor.fetchall()
+    
+#     the_response = make_response(jsonify(theData))
+#     the_response.status_code = 200
+#     return the_response
 
-    the_response = make_response(jsonify(return_dict))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+# #------------------------------------------------------------
+# # Makes use of the very simple ML model in to predict a value
+# # and returns it to the user
+# @customers.route('/prediction/<var01>/<var02>', methods=['GET'])
+# def predict_value(var01, var02):
+#     current_app.logger.info(f'var01 = {var01}')
+#     current_app.logger.info(f'var02 = {var02}')
+
+#     returnVal = predict(var01, var02)
+#     return_dict = {'result': returnVal}
+
+#     the_response = make_response(jsonify(return_dict))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
