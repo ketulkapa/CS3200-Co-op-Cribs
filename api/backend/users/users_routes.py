ab13700504e1b0da61680eae4fdb92fe9cbdc052
aravgoyal <aravgoyal@outlook.com>
2024-12-03 16:06:03 -0500

    added route to return all students

--- api/backend/users/users_routes.py
+++ api/backend/users/users_routes.py
@@ -17,3 +17,36 @@ def get_all_users():
     response = make_response(jsonify(data))
     response.status_code = 200
     return response
+
+# Creates new user
+# @users.route('/users', methods=['POST'])
+# def add_user():
+#     current_app.logger.info('POST /users route')
+#     user_info = request.json
+#     name = user_info['name']
+#     population_density = neighborhood_info.get('population_density', None)
+#     safety_travel = neighborhood_info.get('safety_travel', None)
+#     insights = neighborhood_info.get('insights', None)
+    
+#     query = '''
+#         INSERT INTO neighborhoods (name, population_density, safety_travel, insights)
+#         VALUES (%s, %s, %s, %s)
+#     '''
+#     data = (name, population_density, safety_travel, insights)
+#     cursor = db.get_db().cursor()
+#     cursor.execute(query, data)
+#     db.get_db().commit()
+    
+#     response = make_response(jsonify({'message': 'Neighborhood added successfully!'}))
+#     response.status_code = 201
+#     return response
+
+# Retrieves all users
+@users.route('/users/students', methods=['GET'])
+def get_all_users():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM users')
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
