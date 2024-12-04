60cd5a93dbceda1af4f0c16aa8594e370b22e6fd
arnavm234 <81578361+arnavm234@users.noreply.github.com>
2024-12-04 20:19:34 +0000

    Added retrieve all events GET route

--- api/backend/events/events_routes.py
+++ api/backend/events/events_routes.py
@@ -10,5 +10,15 @@ from flask import current_app
 from backend.db_connection import db
 from backend.ml_models.model01 import predict
 
-# Create a new Blueprint for neighborhoods
+# Create a new Blueprint for events
 neighborhoods = Blueprint('events', __name__)
+
+# Retrieve all events
+@events.route('/events', methods=['GET'])
+def get_all_events():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM events')
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
