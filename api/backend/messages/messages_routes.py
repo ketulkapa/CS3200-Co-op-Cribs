51625a1a5c632178d2832c1069bc12886641ac32
arnavm234 <81578361+arnavm234@users.noreply.github.com>
2024-12-04 20:44:00 +0000

    made messages blueprint

---
+++ api/backend/messages/messages_routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+# Create a new Blueprint for messages
+messages = Blueprint('messages', __name__)
