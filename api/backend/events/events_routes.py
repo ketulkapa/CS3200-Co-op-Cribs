d7bb5306645bbcc91772062bb2067671b935066c
arnavm234 <81578361+arnavm234@users.noreply.github.com>
2024-12-04 20:29:37 +0000

    add event route added

--- api/backend/events/events_routes.py
+++ api/backend/events/events_routes.py
@@ -11,7 +11,7 @@ from backend.db_connection import db
 from backend.ml_models.model01 import predict
 
 # Create a new Blueprint for events
-neighborhoods = Blueprint('events', __name__)
+events = Blueprint('events', __name__)
 
 # Retrieve all events
 @events.route('/events', methods=['GET'])
@@ -21,4 +21,29 @@ def get_all_events():
     data = cursor.fetchall()
     response = make_response(jsonify(data))
     response.status_code = 200
+    return response
+
+# Add a new event
+@events.route('/events', methods=['POST'])
+def add_event():
+    current_app.logger.info('POST /events route')
+    event_info = request.json
+    name = event_info['name']
+    event_date = event_info.get('event_date')
+    loc = event_info.get('loc')
+    insights = event_info.get('description', None)
+    target_audience = event_info.get('target_audience', None)
+    event_host = event_info.get('event_host', None)
+    
+    query = '''
+        INSERT INTO events (name, event_date, loc, description, target_audience, event_host)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    '''
+    data = (name, event_date, loc, description, target_audience, event_host)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    
+    response = make_response(jsonify({'message': 'Event added successfully!'}))
+    response.status_code = 201
     return response
