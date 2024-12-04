287127960af56fd702bebe4d02ee13a83334c1b3
arnavm234 <81578361+arnavm234@users.noreply.github.com>
2024-12-04 20:35:38 +0000

    added update_event

--- api/backend/events/events_routes.py
+++ api/backend/events/events_routes.py
@@ -47,3 +47,31 @@ def add_event():
     response = make_response(jsonify({'message': 'Event added successfully!'}))
     response.status_code = 201
     return response
+
+
+# Update an existing event by ID
+@events.route('/events/<int:event_id>', methods=['PUT'])
+def update_event(event_id):
+    current_app.logger.info(f'PUT /events/{event_id} route')
+    event_info = request.json
+    name = event_info['name']
+    event_date = event_info.get('event_date')
+    loc = event_info.get('loc')
+    insights = event_info.get('description', None)
+    target_audience = event_info.get('target_audience', None)
+    event_host = event_info.get('event_host', None)
+    
+    query = '''
+        UPDATE events
+        SET name = %s, event_date = %s, loc = %s, description = %s, target_audience = %s, event_host = %s
+        WHERE event_id = %s
+    '''
+
+    data = (event_id, name, event_date, loc, description, target_audience, event_host)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+
+    response = make_response(jsonify({'message': 'Event updated successfully!'}))
+    response.status_code = 200
+    return response
