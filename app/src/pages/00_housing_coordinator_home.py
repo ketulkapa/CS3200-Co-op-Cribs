033fa813ffa55c3a13aaca58b64c4524d445b3d0
ketulkapa <kapadia.ket@northeastern.edu>
2024-12-04 16:50:28 -0500

    feat: update navigation buttons and add new actions for listings and users

--- app/src/pages/00_housing_coordinator_home.py
+++ app/src/pages/00_housing_coordinator_home.py
@@ -14,12 +14,27 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('View World Bank Data Visualization', 
+if st.button('View Dashboards', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/01_World_Bank_Viz.py')
+  st.switch_page('pages/01_View_Dashboards.py')
 
-if st.button('View World Map Demo', 
+if st.button('Update a Listing',
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_Map_Demo.py')
+  st.switch_page('pages/03_Update_Listing.py')
+
+if st.button('Delete a Listing',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/04_Delete_Listing.py')
+
+if st.button('View All Users', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_View_Users.py')
+
+if st.button('View All Recently Posted Listings',
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/05_Recent_Listings.py')
